ba1f01d393c124fe7623c0117aea4d5d6344cefb
it24103818 <it24103818@my.sliit.lk>
2025-05-20 13:37:36 +0530

    Main class with SB Application Annotation

---
+++ src/main/java/com/drivinglearners/driving_learners/DrivingLearnersApplication.java
@@ -0,0 +1,14 @@
+package com.drivinglearners.driving_learners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingLearnersApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DrivingLearnersApplication.class, args);
+	}
+
+}
