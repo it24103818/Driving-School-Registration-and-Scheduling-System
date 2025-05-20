b02b97219b5bbb822897dfa29243c5e085eb148a
IT24104129 <it24104129@my.sliit.lk>
2025-05-20 13:29:22 +0530

    SearchParameters encapsulated

---
+++ src/main/java/com/drivinglearners/driving_learners/model/SearchCriteria.java
@@ -0,0 +1,13 @@
+package com.drivinglearners.driving_learners.model;
+
+public class SearchCriteria {
+    private String id;
+    private String email;
+
+    public SearchCriteria() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
